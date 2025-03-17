fcc7db9cec29343d2bc9c3182344b23d0dd3ddd1
unknown <rodrieldk1@gmail.com>
2025-03-17 14:42:30 -0300

    avr si va

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,7 +1,7 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { c as createExports } from './chunks/entrypoint_Dj1YljEH.mjs';
-import { manifest } from './manifest_DswUHYd1.mjs';
+import { manifest } from './manifest_Dpk30ARR.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -20,7 +20,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "3d81a054-d66f-4a35-9a00-63443a330564",
+    "middlewareSecret": "abe6d66e-0712-484e-b346-a180508c9f7e",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
